2124a69f820270144f23509f078c7b1427661ca7
lanou3g <871192135@qq.com>
2016-04-19 19:51:03 +0800

    视频部分上传

---
+++ Spice/Classes/Main/Tool/LQNetWoringRequestManager.h
@@ -0,0 +1,13 @@
+//
+//  LQNetWoringRequestManager.h
+//  Spice
+//
+//  Created by lanou3g on 16/4/16.
+//  Copyright © 2016年 spice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LQNetWoringRequestManager : NSObject
+
+@end
---
+++ Spice/Classes/Main/Tool/LQNetWoringRequestManager.m
@@ -0,0 +1,13 @@
+//
+//  LQNetWoringRequestManager.m
+//  Spice
+//
+//  Created by lanou3g on 16/4/16.
+//  Copyright © 2016年 spice. All rights reserved.
+//
+
+#import "LQNetWoringRequestManager.h"
+
+@implementation LQNetWoringRequestManager
+
+@end
