1027bb34a9e63f4b5f83526bd774ba3cd8fb8f38
spice <shijiannan1314@qq.com>
2016-04-20 19:27:36 +0800

    sjn 第二次上传

--- Spice/Classes/Joke/Controller/LQJokeTableViewController.m
+++ Spice/Classes/Joke/Controller/LQJokeTableViewController.m
@@ -10,13 +10,20 @@
 #import "LQtJokeTableViewCell.h"
 #import "LQModel.h"
 #import "AppDelegate.h"
+#import "MJRefresh.h"
+#import "LQImageCoViewController.h"
+
+#define URL(num) @"http://duanzi.duowan.com/index.php?r=duatopicList&page=1&pageSize=30&sort=2"
 @interface LQJokeTableViewController ()
 {
-    BOOL isHave;
+    BOOL isHave ;
 }
 @property(nonatomic,strong)NSMutableDictionary *dic;
 @property(nonatomic,strong)NSMutableArray *sarry;
 @property(nonatomic,strong)UIImageView *bigimage;
+@property(nonatomic,assign)int num;
+@property(nonatomic,assign)CGFloat currentScale;
+@property(nonatomic,strong)UIWindow *window;
 
 @end
 
@@ -30,16 +37,44 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    NSMutableArray *array1 =[NSMutableArray array];
-//    self.sarry =[[NSMutableArray array]copy];
+    isHave= YES;
+    self.num = 2;
     self.tableView.rowHeight = 400;
     self.dic =[[NSMutableDictionary dictionary]mutableCopy];
-    [self AFHTTP];
-    isHave = YES;
-    
+    [self.tableView  addHeaderWithTarget:self action:@selector(AFHTTP)];
+    [self.tableView addFooterWithTarget:self action:@selector(AFHTTPLOAD)];
+    [self.tableView headerBeginRefreshing];
 
 }
+-(void)AFHTTPLOAD{
+    AFHTTPSessionManager *manager =[AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html",nil];//设置相应内容类型
+    [manager GET:[NSString stringWithFormat:@"http://duanzi.duowan.com/index.php?r=duanzi/topicList&page=%d&pageSize=30&sort=2",self.num] parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSMutableDictionary *dic2 =[NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers | NSJSONReadingMutableLeaves error:nil];
+            self.num++;
+        NSArray *arry = [[dic2 objectForKey:@"result"]objectForKey:@"list"];
+        for (NSDictionary *dic in arry) {
+            LQModel *modellist =[[LQModel alloc]init];
+            [modellist setValuesForKeysWithDictionary:dic];
+            [self.sarry addObject:modellist];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+            
+        });
+//        [self.tableView  headerEndRefreshing];
+        [self.tableView  footerEndRefreshing];
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+    }];
+}
 -(void)AFHTTP{
+    [self requestData];
+}
+-(void)requestData{
     AFHTTPSessionManager *manager =[AFHTTPSessionManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html",nil];//设置相应内容类型
@@ -57,6 +92,7 @@
             [self.tableView reloadData];
             
         });
+        [self.tableView  headerEndRefreshing];
         
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
     }];
@@ -88,26 +124,71 @@
     [cell.headImage sd_setImageWithURL:[NSURL URLWithString:model.avatar]];
     cell.MainLabel.text = model.content;
     [cell.MainImage sd_setImageWithURL:[NSURL URLWithString:[[model.imagelist objectAtIndex:0] objectForKey:@"large"]]];
-//    [self.bigimage sd_setImageWithURL:[NSURL URLWithString:[[model.imagelist objectAtIndex:0] objectForKey:@"large"]]];
     cell.MainImage.userInteractionEnabled =YES;
-    UITapGestureRecognizer *tap =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clickimage:)];
-    [cell.MainImage addGestureRecognizer:tap];
+//    if (isHave == YES) {
+        UITapGestureRecognizer *tap =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clickimage:)];
+        [cell.MainImage addGestureRecognizer:tap];
+//        isHave = NO;
+//    }
+    
+    
     return cell;
 }
 -(void)clickimage:(UITapGestureRecognizer*)tap{
-//    UIView *MainView =tap.view;
-    NSLog(@"111");
-//    MainView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-    UIView *mainview =[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-    tap.view.frame = CGRectMake(40, 60, self.view.frame.size.width-80, self.view.frame.size.height-120);
-    mainview.backgroundColor =[UIColor blackColor];
-    mainview.alpha = 0.8;
-    UIWindow *window =[UIApplication sharedApplication].keyWindow;
-    [window addSubview:mainview];
-    [window addSubview:tap.view];
-//
+    LQImageCoViewController *lqimagec =[[LQImageCoViewController alloc]init];
+    [self presentViewController:lqimagec animated:nil completion:^{
+        
+    }];
+//    if (isHave == NO) {
+//        UIView *mainview =[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+//        tap.view.frame = CGRectMake(0, 60, self.view.frame.size.width, self.view.frame.size.height-120);
+//        mainview.backgroundColor =[UIColor blackColor];
+//        mainview.alpha = 0.8;
+////        self.window =[UIApplication sharedApplication].keyWindow;
+////        [_window addSubview:mainview];
+//        
+////        [_window removeFromSuperview];
+////        [_window addSubview:tap.view];
+//        UIPinchGestureRecognizer *gesture= [[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(scaleimage:)];
+//        [tap.view addGestureRecognizer:gesture];
+//        isHave = YES;
+//        UITapGestureRecognizer *taprelese= [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(backToCell)];
+//        [_window addGestureRecognizer:taprelese];
+//    }
+
     
 }
+
+-(void)backToCell{
+    NSLog(@"11111");
+
+    
+}
+//-(void)scaleimage:(id)sender{
+//
+//        
+//        [self.view bringSubviewToFront:[(UIPinchGestureRecognizer*)sender view]];
+//        
+//        //当手指离开屏幕时,将lastscale设置为1.0
+//        
+//        if([(UIPinchGestureRecognizer*)sender state] == UIGestureRecognizerStateEnded) {
+//            
+//            self.currentScale = 1.0;
+//            
+//            return;
+//        }
+//        CGFloat scale = 1.0 - (self.currentScale - [(UIPinchGestureRecognizer*)sender scale]);
+//        
+//        CGAffineTransform currentTransform = [(UIPinchGestureRecognizer*)sender view].transform;
+//        
+//        CGAffineTransform newTransform = CGAffineTransformScale(currentTransform, scale, scale);
+//        
+//        [[(UIPinchGestureRecognizer*)sender view] setTransform:newTransform];
+//        
+//        self.currentScale = [(UIPinchGestureRecognizer*)sender scale];
+//        
+//    }
+
 
 
 
